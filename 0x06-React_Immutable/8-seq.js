1f228dfa2ef9b090908879d9448674f47979a652
scholastica25 <schogbonnamara@gmail.com>
2024-07-18 18:59:09 +0000

    react immutable updated

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,7 +1,21 @@
-import immutable from 'immutable';
+import { Seq } from 'immutable';
 
-const { Seq } = immutable;
+const printBestStudents = (object) => {
+  const seq = Seq(object);
 
-export default function printBestStudents() {
-    
-}
+  const getStudent = seq.filter((value) => value.score > 70);
+
+  const student = getStudent.toJS();
+
+  const formatName = (name) => name.charAt(0).toUpperCase() + name.slice(1);
+
+  Object.keys(student).map((key) => {
+    student[key].firstName = formatName(student[key].firstName);
+    student[key].lastName = formatName(student[key].lastName);
+    return student[key];
+  });
+
+  console.log(student);
+};
+
+export default printBestStudents;
